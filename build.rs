2589099e21940aac4c6ae48adbf67b47e975a34f
Wison Ye <wisonye@gmail.com>
2022-08-15 10:09:14 +1200

    [ master ] - Added useful comment to 'build.rs'.

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
-// build.rs
-
-use std::env;
-
+///
+/// Let the compiler to link the installed `raylib` libraries
+///
+/// MacOS: `brew install raylib`
+///
 fn main() {
 
     println!("cargo:rustc-link-lib=raylib"); // the "-l" flag
